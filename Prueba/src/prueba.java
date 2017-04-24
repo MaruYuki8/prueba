3880558560dae983628b3e849b1ddbbe6d011f8e
orphench <cris_alchemist8@hotmail.com>
2017-04-24 17:50:25 -0300

    primer hola mundo

---
+++ Prueba/src/prueba.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+
+
+public class prueba {
+    
+    public static void main (String [] args){
+      System.out.println("hola mundo");   
+      
+    }
+    
+   
+    
+}
